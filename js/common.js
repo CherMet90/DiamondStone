ab1b2d57f62699eef2482e76757bd9aa4d6b0a13
CherMet <dddfrdd@gmail.com>
2018-03-02 20:54:26 +0400

    nav

--- js/common.js
+++ js/common.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-	$(".nav-item").click(function() {
-		$(".nav-item").removeClass("active").eq($(this).index()).addClass("active");
+
+	$(".nav__logo--wrap").css('max-width', $(window).width());
+	$(window).resize(function(){
+		$(".nav__logo--wrap").css('max-width', $(window).width());
+	});
+	
+	$(".first-row").click(function() {
+		$(".nav-item").removeClass("active")
+		$(".first-row").eq($(this).index()).addClass("active");
+	})
+	$(".second-row").click(function() {
+		$(".nav-item").removeClass("active")
+		$(".second-row").eq($(this).index()).addClass("active");
 	})
 })
