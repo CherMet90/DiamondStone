03db5c83b224933d12167f58131fa45579cf588c
CherMet <dddfrdd@gmail.com>
2018-03-21 19:58:11 +0400

    carousel

--- js/common.js
+++ js/common.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+  
+ //  heightAlignment(".carousel-item");
+
+	// $(window).resize(function(){
+	// 	heightAlignment(".carousel-item");
+	// });
+
 	$(".first-row").click(function() {
 		$(".nav-item").removeClass("active")
 		$(".first-row").eq($(this).index()).addClass("active");
@@ -7,4 +14,17 @@ $(document).ready(function() {
 		$(".nav-item").removeClass("active")
 		$(".second-row").eq($(this).index()).addClass("active");
 	})
+
+	function heightAlignment (elemsName) {
+		$(elemsName).css('height', '');
+		var max = $(elemsName).eq(0).height();
+		$(elemsName).each(function() {
+			if ($(this).height() > max) {
+				max = $(this).height();
+			};
+		});
+		$(elemsName).each(function() {
+			$(this).css('height', max);
+		});
+	};
 })
